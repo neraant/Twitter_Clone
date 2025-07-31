9e8ac822b06b7ab6f628eee394334a33aa30da71
neraant <neraant1111@gmail.com>
2025-07-31 14:55:43 +0300

    fix: added env var to ci

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from '@playwright/test';
 
+import { BASE_URL } from '@/shared/lib/common';
+
 export default defineConfig({
   testDir: './e2e',
   globalSetup: './e2e/setup.ts',
   workers: 1,
   use: {
-    baseURL: 'http://localhost:3000',
+    baseURL: BASE_URL,
     storageState: undefined,
     headless: true,
     contextOptions: {
